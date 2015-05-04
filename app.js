3cda88f205f8b937509ffd672bbf7111d7544083
mec64 <masonecrane@gmail.com>
2015-05-04 14:39:27 -0600

    downloaded 3 car images, started to flesh out the node server, and now creating the /cars page

---
+++ app.js
@@ -0,0 +1,56 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+//    console.log('hello');
+//    console.log('REQUEST OBJECT', request);
+    if(request.url === '/cars') {
+        fs.readFile('cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/style.css') {
+        fs.readFile('./stylesheets/style.css', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/css'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/cats') {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/c1.jpeg') {
+        fs.readFile('./images/c1.jpeg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/jpeg'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if(request.url === '/cars/new') {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('URL requested cannot be found!');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// print to terminal window
+console.log("Running in localhost at port 6789");
